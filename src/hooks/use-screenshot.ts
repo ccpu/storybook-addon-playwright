6eb55e7e5079c9986211bf941f1e59bbaf506252
Cyrus-d <m.doaie@hotmail.com>
2020-05-11 10:09:47 +0430

    fixed useScreenshot

--- src/hooks/use-screenshot.ts
+++ src/hooks/use-screenshot.ts
@@ -1,12 +1,12 @@
-import { useState, useCallback, useRef } from 'react';
+import { useState, useCallback, useRef, useEffect } from 'react';
 import { useKnobs } from './use-knobs';
 import { useStorybookState } from '@storybook/api';
 import { getSnapShot } from '../api/client';
 import { BrowserTypes } from '../typings';
 import { GetScreenshotResponse } from '../api/typings';
 import sum from 'hash-sum';
-import useDebounce from 'react-use/lib/useDebounce';
 import { useCurrentActions } from './use-current-actions';
+// import useUnmount from 'react-use/lib/useUnmount';
 
 export const useScreenshot = (browserType: BrowserTypes | 'storybook') => {
   const [screenshot, setScreenshotInfo] = useState<GetScreenshotResponse>();
@@ -15,6 +15,8 @@ export const useScreenshot = (browserType: BrowserTypes | 'storybook') => {
 
   const { currentActions } = useCurrentActions();
 
+  // useUnmount(() => alert('UNMOUNTED'));
+
   const prevKnobs = useRef();
   const prevActions = useRef();
 
@@ -22,40 +24,46 @@ export const useScreenshot = (browserType: BrowserTypes | 'storybook') => {
   const getSnapshot = useCallback(() => {
     if (browserType === 'storybook') return;
     setLoading(true);
-
+    console.log('currentActionHash');
     getSnapShot({
       actions: currentActions,
       browserType,
       knobs,
       storyId: state.storyId,
-    }).then((snapShotsInfo) => {
-      setLoading(false);
-      setScreenshotInfo(snapShotsInfo);
-    });
+    })
+      .then((snapShotsInfo) => {
+        setScreenshotInfo(snapShotsInfo);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, [browserType, currentActions, knobs, state.storyId]);
 
-  const [,] = useDebounce(
-    () => {
-      if (loading) return;
+  useEffect(() => {
+    if (loading) return;
+    const currentActionHash = sum(currentActions);
+    const currentKnobHash = sum(knobs);
 
-      const currentActionHash = sum(currentActions);
-      const currentKnobHash = sum(knobs);
+    if (
+      prevKnobs.current === currentKnobHash &&
+      prevActions.current === currentActionHash
+    ) {
+      return;
+    }
 
-      if (
-        prevKnobs.current === currentActionHash &&
-        prevActions.current === currentKnobHash
-      ) {
-        return;
-      }
+    prevKnobs.current = currentKnobHash;
+    prevActions.current = currentActionHash;
 
-      prevKnobs.current = currentKnobHash;
-      prevActions.current = currentActionHash;
+    getSnapshot();
+  }, [currentActions, getSnapshot, knobs, loading]);
 
-      getSnapshot();
-    },
-    50,
-    [knobs, prevKnobs, getSnapshot, prevActions, currentActions, browserType],
-  );
+  // const [,] = useDebounce(
+  //   () => {
+
+  //   },
+  //   50,
+  //   [knobs, prevKnobs, getSnapshot, prevActions, currentActions, browserType],
+  // );
 
   return { getSnapshot, loading, screenshot };
 };

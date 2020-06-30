7ca4712d62ee975e0496c6d970ad61b8a2c8b610
ccpu <m.doaie@hotmail.com>
2020-06-30 18:38:58 +0430

    fixed config

--- src/typings/config.ts
+++ src/typings/config.ts
@@ -6,7 +6,7 @@ import { PageMethodKeys } from '../api/server/services/typings';
 export interface Config<T extends unknown = Page> {
   storybookEndpoint: string;
   customActionSchema?: ActionSchemaList;
-  pageMethods: PageMethodKeys[];
+  pageMethods?: PageMethodKeys[];
   getPage: (
     browserType: BrowserTypes,
     deviceDescriptor: DeviceDescriptor,

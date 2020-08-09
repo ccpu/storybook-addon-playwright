a4b5da497b364da42e1ae6711a3a489bf8a41a1b
ccpu <m.doaie@hotmail.com>
2020-08-09 18:40:27 +0430

    fixed useGlobalActionDispatch

--- src/hooks/use-global-action-dispatch.ts
+++ src/hooks/use-global-action-dispatch.ts
@@ -1,8 +1,39 @@
 import { useGlobalState } from './use-global-state';
 import { Action } from '../store/actions/reducer';
+import { useCallback, useEffect } from 'react';
 
 export const useGlobalActionDispatch = () => {
   const [action, dispatch] = useGlobalState<Action>('action-dispatch');
 
   return { action, dispatch };
+};
+
+type Dispatch = (action: Action) => void;
+
+const dispatches: { [id: string]: Dispatch } = {};
+
+export const useGlobalActionDispatch2 = (id: string, callback?: Dispatch) => {
+  if (callback) {
+    dispatches[id] = callback;
+  }
+
+  const dispatch = useCallback(
+    (action?: Action) => {
+      if (!dispatches[id]) {
+        throw new Error('Dispatch id not registered yet!');
+      }
+      dispatches[id](action);
+    },
+    [id],
+  );
+
+  useEffect(() => {
+    return () => {
+      if (dispatches[id]) {
+        delete dispatches[id];
+      }
+    };
+  }, [id]);
+
+  return { dispatch };
 };

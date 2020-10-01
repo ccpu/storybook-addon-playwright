6eb783cc296b93fa2fb8af562fcecbdb4ecda405
ccpu <m.doaie@hotmail.com>
2020-10-01 14:23:02 +0330

    fixed knob array

--- src/utils/__tests__/knobs-to-querystring.test.ts
+++ src/utils/__tests__/knobs-to-querystring.test.ts
@@ -14,4 +14,10 @@ describe('knobsToQuerystring', () => {
       'knob-prop1=%7B%22prop2%22:%22val-2%22%7D',
     );
   });
+
+  it('should handle array', () => {
+    expect(knobsToQuerystring({ prop1: { prop2: ['foo', 'bar'] } })).toBe(
+      'knob-prop1=%7B%22prop2%22:%5B%22foo%22,%22bar%22%5D%7D',
+    );
+  });
 });
--- src/utils/knobs-to-querystring.ts
+++ src/utils/knobs-to-querystring.ts
@@ -6,9 +6,15 @@ export const knobsToQuerystring = (props?: ScreenshotProp) => {
   const keys = Object.keys(props);
 
   const propQuery = keys.map((prop) => {
+    if(Array.isArray(props[prop]))
+    {
+        return `knob-${prop}=${props[prop].join(',')}`;
+    }
+
     if (typeof props[prop] === 'object') {
       return `knob-${prop}=${JSON.stringify(props[prop])}`;
     }
+
     return `knob-${prop}=${props[prop]}`;
   });
   return encodeURI(propQuery.join('&'));
